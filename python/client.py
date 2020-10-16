dbe825b3dc109d6d75a89935f69a59a49a1bd8ae
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2020-10-16 01:31:32 -0500

    Initial commit

---
+++ python/client.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+import pickle
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+def generate_credentials(client_secrets_file='client_secret.json'):
+    """ Get credentials """
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file, scopes)
+    credentials =  flow.run_console()
+    pickle.dump(credentials, open('credentials.pkl','wb'))
+    return credentials
+
+    
+def get_credentials(filename='credentials.pkl'):
+    if os.path.exists(filename):
+        return pickle.load(open(filename,'rb'))
+    else:
+        return generate_credentials() 
+    
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    # Get credentials and create an API client
+    credentials = get_credentials()
+    
+    # pickle.dump(creds, open('credentials.pkl','wb'))
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.playlists().list(
+        part="contentDetails, id, localizations,player,snippet,status",
+        mine=True
+    )
+    response = request.execute()
+
+  
+    return response
+
+if __name__ == "__main__":
+    creds = main()
