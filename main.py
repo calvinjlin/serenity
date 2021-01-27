b7ab74cb9c32ba6b22f082aa3724500e109d1944
Calvin J Lin <58328424+calvinjlin@users.noreply.github.com>
2021-01-27 07:32:33 -0600

    refactor, try adding gui

--- main.py
+++ main.py
@@ -15,34 +15,33 @@ import googleapiclient.errors
 #     def __init__(self):
         
 class YTConnector:
-    def build_credentials(self, client_secrets_file='client_secret.json'):
-        """ Get credentials """
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-                client_secrets_file, self.scopes)
-        credentials =  flow.run_console()
-        pickle.dump(credentials, open('credentials.pkl','wb'))
-        return credentials
-    
-    def find_credentials(self, filename='credentials.pkl'):
-        if os.path.exists(filename):
-            return pickle.load(open(filename,'rb'))
-        else:
-            return self.build_credentials() 
-
-    def __init__(self):
+    def __init__(self) -> None:
+        super().__init__()
         # Disable OAuthlib's HTTPS verification when running locally.
         # *DO NOT* leave this option enabled in production.
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    
-        api_service_name = "youtube"
-        api_version = "v3"
+
+        self.api_service_name = 'youtube'
+        self.api_version = 'v3'
         self.scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-        
-        self.credentials = self.find_credentials()
+
+        self.client_secrets_file = 'client_secret.json'
+
         self.youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, credentials=self.credentials)
+            self.api_service_name, self.api_version, credentials=self.user_oauth_approval())
         
-    def list_my_playlists(self):
+    def user_oauth_approval(self, filename='credentials.pkl'):
+        # User is asked to grant app permission to access their youtube account, permission is saved to file
+        if os.path.exists(filename):
+            credentials = pickle.load(open(filename,'rb'))
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                self.client_secrets_file, self.scopes)
+            credentials =  flow.run_console()
+            pickle.dump(credentials, open(filename,'wb'))
+        return credentials
+        
+    def my_playlists(self):
         responses=[]
         def send_request(nextPageToken=None):
             request = self.youtube.playlists().list(
@@ -87,7 +86,8 @@ class YTConnector:
         
 def main():
     yt = YTConnector()
-    return yt.list_my_playlists()
+    return yt.my_playlists()
 
 if __name__ == "__main__":
     yt = main()
+    print(yt)
